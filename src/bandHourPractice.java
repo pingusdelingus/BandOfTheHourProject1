495b688499608424a5720bba90d0f4261c2b6df3
pingusdelingus <alphaspammail2016@gmail.com>
2024-02-25 21:09:26 -0500

    1st band Hour Practice commit

--- src/bandHourPractice.java
+++ src/bandHourPractice.java
@@ -8,51 +8,60 @@ public class bandHourPractice {
 
 
     public static void main(String[] args) {
-      int numOfRows, index, jdex;
+        int numOfRows,
 
         numOfRows = 0;
-        // ask user for # of rows
+        char rowChar, userDecision;
+
+        rowChar = ROW_START_VAL;
+
+
         System.out.println("Welcome to the Band of the Hour");
         System.out.println("-------------------------------");
+
+        //calls getNumOfRows method to retrieve the number of rows from the user.
         numOfRows = getNumOfRows(numOfRows);
+        //print check
         System.out.println("The number of rows is :" + numOfRows);
 
 
         // Initializes jagged array with numOfRows # of rows
 
-        int[][] bandSetup = new int[numOfRows][];
-
-        setupPositionsInRow(bandSetup, numOfRows);
-        // displaying contents of the array created :D
-        System.out.println("Contents of the 2D jagged array");
-        for (index = 0; index < bandSetup.length; index++){
-            for (jdex = 0; jdex < bandSetup[index].length; jdex++){
-                System.out.println(bandSetup[index][jdex]);
-            }
-            System.out.println();
-        }
+        double[][] bandSetup = new double[numOfRows][];
+        // call method to setup size of jagged array
+        setupPositionsInRow(bandSetup, numOfRows, rowChar);
 
 
+        // call method to display the jagged array for the user in the console
+        do {
+            displayJaggedArray(bandSetup, rowChar);
 
 
-
-
-
-
-
-
+            System.out.println("(A)dd, (R)emove, (P)rint,          e(X)it : p");
+            userDecision = keyboard.next().charAt(0);
+            switch (userDecision){
+                case 'A':
+                    // run addBandMember METHOD
+                    break;
+                case 'R':
+                    //run removeBandMember METHOD
+                    break;
+                case 'X':
+                    //exit code and end main METHOD
+                    break;
+            }// end of switch statement
+        } while (keyboard.next().charAt(0) != 'X');
 
 
     }// end of the main METHOD
 
-    private static int getNumOfRows(int numberRows){
+    private static int getNumOfRows(int numberRows) {
         System.out.println("Please enter number of rows");
         numberRows = keyboard.nextInt();
-        do{
-            if (numberRows > 0 && numberRows <= MAX_ROWS){
+        do {
+            if (numberRows > 0 && numberRows <= MAX_ROWS) {
                 return numberRows;
-            }
-            else {
+            } else {
                 System.out.println("ERROR: Out of range, try again");
                 numberRows = keyboard.nextInt();
             }
@@ -62,32 +71,51 @@ public class bandHourPractice {
 
     }// end of the getNumOfRows METHOD
 
-    private static void setupPositionsInRow(int[][] bandSetup, int numOfRows){
-    //sets up the jagged array values
-        int index;
-        char rowChar;
-        int numPositions;
-        rowChar = ROW_START_VAL;
-        for (index = 0; index < numOfRows; index++){
-            rowChar = (char) (rowChar + index);
-            System.out.println("Please enter the number of positions in row" + rowChar);
+    private static void setupPositionsInRow(double[][] bandSetup, int numOfRows, char rowChar) {
+        //sets up the jagged array values
+        int index, numPositions;
+
+
+
+        for (index = 0; index < numOfRows; index++) {
+            rowChar = (char) (ROW_START_VAL + index);
+            System.out.println("Please enter the number of positions in row " + rowChar);
             numPositions = keyboard.nextInt();
-            while(numPositions < 0 || numPositions > MAX_POSITIONS){
+            while (numPositions < 0 || numPositions > MAX_POSITIONS) {
                 System.out.println("ERROR: Out of range, try again");
                 numPositions = keyboard.nextInt();
             }
-            bandSetup[index] = new int[numPositions];
+
+            bandSetup[index] = new double[numPositions];
 
 
         }// end of the for loop
 
 
-
-
-
-
-
     }// end of the setupPositionsInRow METHOD
+
+    private static void displayJaggedArray(double[][] bandSetup, char charDisplay) {
+        // displaying contents of the array created :D
+        int index, jdex;
+
+
+        System.out.println("Current Band of the Hour Arrangement :D");
+        for (index = 0; index < bandSetup.length; index++) {
+            charDisplay = (char) (ROW_START_VAL + index);
+            System.out.print(charDisplay + ":   ");
+            for (jdex = 0; jdex < bandSetup[index].length; jdex++) {
+
+                System.out.print(bandSetup[index][jdex]);
+                System.out.print("   ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+
+    }// end of the displayJaggedArray METHOD
+
+
 
 
 }// end of the bandHourPractice CLASS
